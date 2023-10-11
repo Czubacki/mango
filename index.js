cebf43dedd239ccaf133ef6179136bf60709fdd8
Czubacki <filipczubacki@wp.pl>
2023-10-11 14:02:20 +0200

    working

--- index.js
+++ index.js
@@ -4,10 +4,26 @@ const { MongoClient } = require('mongodb');
 const client = new MongoClient(mongoString);
 
 async function main() {
+    try {
+        await client.connect();
 
-await client.connect();
+        await listDB(client);
+    } catch (error) {
+            console.error(error);
+    } finally {
+        await client.close();
+    }
 
-await client.close();
+}
+
+async function listDB(client) {
+    let databaseList = await client.db().admin().listDatabases();
+
+    console.log("Lista baz danych:")
+    
+    databaseList.databases.forEach(database => {
+     console.log("Nazwa: " + database.name + ", Rozmair: " + database.sizeOnDisk);
+    });
 }
 
 main();
